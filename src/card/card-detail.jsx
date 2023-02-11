c47c8b2566f03233da6ee8da6addeb20e2a66dd1
Gerard Press <killerabs@gmail.com>
2023-02-11 08:09:19 +0000

    new card ui

---
+++ src/card/card-detail.jsx
@@ -0,0 +1,16 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Stack,
+  Heading,
+  Divider,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
+
+const CardDetail = (car) => {};
+
+export default CardDetail;
